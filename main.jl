7fc4ff635817e5bb8c0c1b883fab8d2400ac3b42
meysam-kazemi <meisam.daysu@gmail.com>
2023-03-24 06:30:10 +0330

    step2 func

--- main.jl
+++ main.jl
@@ -10,16 +10,45 @@ r3 = shuffle(alphabet)
 function rotor(text,r)
     res = ""
     splited_list  = split(text,"")
-    for i in 1:length(splited_list)
-        index = findfirst(x->x==splited_list[i],alphabet)
+    for i in splited_list
+        index = findfirst(x->x==i,alphabet)
         res *= r[index]
     end
-end
-function ()
-    
+    return res
 end
 
+function rotor!(text,r)
+    res = ""
+    splited_list  = split(text,"")
+    for i in splited_list
+        index = findfirst(x->x==i,r)
+        res *= alphabet[index]
+    end
+    return res
+end
 
+function step1(text)
+    t1 = rotor(text,r1)
+    t2 = rotor(t1,r2)
+    t3 = rotor(t2,r3)
+    return t3
+end
+function inverse(text)
+    res = ""
+    splited_list  = split(text,"")
+    for i in splited_list
+        index = findfirst(x->x==i,alphabet)
+        res *= alphabet[end-index+1]
+    end
+    return res
+end
+
+function step2(text)
+    t1 = rotor!(text,r1)
+    t2 = rotor!(t1,r2)
+    t3 = rotor!(t2,r3)
+    return t3
+end
 
 
 
