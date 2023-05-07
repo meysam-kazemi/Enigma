e6c734e523d24a224ebac1d7ca25aa6a7729c4d5
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-08 03:29:26 +0330

    start create rotors

--- create_rotors.jl
+++ create_rotors.jl
@@ -1,6 +1,18 @@
 import Shuffle , Random
 Random.seed!(1234);
-a = "12345"
-b = join(Shuffle.shuffle(a),"")
+
+# q = Dict(a[i]=>b[i] for i in 1:length(a))
+
+alphabet = "abcdefghigklmnopqrstuvwxyz"
+
+rotor1 = join(Shuffle.shuffle(alphabet),"")
+println(rotor1)
+
+rotor2 = join(Shuffle.shuffle(alphabet),"")
+println(rotor2)
+
+rotor3 = join(Shuffle.shuffle(alphabet),"")
+println(rotor3)
+
 
 
