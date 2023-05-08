4ccf2f7ce0dc49335a7b3cd960be007c45962118
meysam-kazemi <meisam.daysu@gmail.com>
2023-05-08 05:23:55 +0330

    Assemble the rotors

--- main.jl
+++ main.jl
@@ -6,8 +6,8 @@ rotor1,rotor2,rotor3 = split(rotors,",")
 
 
 # Reflector
-reverse_alphabet = reverse(alphabet[1:end-1])
-reflector = Dict(alphabet[i]=>reverse_alphabet[i] for i in 1:len_alphabet-1)
+reverse_alphabet = reverse(alphabet)
+reflector = Dict(alphabet[i]=>reverse_alphabet[i] for i in 1:len_alphabet)
 
 # Rotation function
 function Rotate_rotor(rotor)
@@ -19,28 +19,26 @@ function Enigma(text)
     global rotor1,rotor2,rotor3 , step
     res = ""
     for c in text
-        # println(c)
-        c1 = rotor1[findfirst(c,rotor1)] # Apply the rotor1 to c(each character)
-        c2 = rotor3[findfirst(c1,rotor2)] # Apply the rotor3 to c
-        c3 = rotor2[findfirst(c2,rotor3)] # Apply the rotor2 to c
+        c1 = rotor1[findfirst(c,alphabet)] # Apply the rotor1 to c(each character)
+        c2 = rotor2[findfirst(c1,alphabet)] # Apply the rotor3 to c
+        c3 = rotor3[findfirst(c2,alphabet)] # Apply the rotor2 to c
         reflect = reflector[c3] 
         ref_c3 = alphabet[findfirst(reflect,rotor3)]  # Apply the reverse of rotor3 to c
         ref_c2 = alphabet[findfirst(ref_c3,rotor2)] #  Apply the reverse of rotor2 to c
-        println(ref_c2,typeof(ref_c2))
         ref_c1 = alphabet[findfirst(ref_c2,rotor1)] #  Apply the reverse of rotor1 to c
-        res * ref_c1 # Add the obtained characet to res
-        rotor1 = Rotate_rotor(rotor1)
-        if (step % 6) == 0 
-            rotor2 = Rotate_rotor(rotor2)
-        elseif (step % 6^2) == 0
-            rotor3 = Rotate_rotor(rotor3)
-        end
-        step += 1
+        res = res * ref_c1 # Add the obtained characet to res
+        # rotor1 = Rotate_rotor(rotor1)
+        # if (step % 6) == 0 
+        #     rotor2 = Rotate_rotor(rotor2)
+        # elseif (step % 6^2) == 0
+        #     rotor3 = Rotate_rotor(rotor3)
+        # end
+        # step += 1
     end
     return res
 end
 
 
-t = Enigma("helloworld")
+t = Enigma("hello world")
 println(Enigma(t))
 
